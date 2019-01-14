23a6010d3ea7accf864dbb94f7b291529025a287
zhangq@webyun.cn <zhangq@webyun.cn>
2019-01-14 09:31:46 +0800

    提交项目

---
+++ src/main/java/com/wechat/springbootwechat/test.java
@@ -0,0 +1,13 @@
+package com.wechat.springbootwechat;
+
+/**
+ * @Description: java类作用描述
+ * @Author: zhangqiang
+ * @CreateDate: 2019/1/14 9:30
+ * @Version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("niha");
+    }
+}
